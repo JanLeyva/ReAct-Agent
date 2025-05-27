d37ad613d7cb6a0dad700ebbab7755cc093fb904
JanLeyva <janleyvamassague@gmail.com>
2025-05-27 14:59:49 +0200

    deleted micro-services load_restaurants and search_engine -> now in monholitic

--- services/load_places/src/config.py
+++
@@ -1,9 +0,0 @@
-from pydantic_settings import BaseSettings, SettingsConfigDict
-
-
-class Config(BaseSettings):
-    model_config = SettingsConfigDict(env_file=".env", env_file_encoding="utf-8")
-    googlemaps_api_key: str
-
-
-config = Config()
