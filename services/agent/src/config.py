f8bb79407716861cabdec4f482f43f8c00cb7e9f
Jan Leyva Massagué <janleyvamassague@gmail.com>
2025-06-20 11:41:01 +0200

    clean env var

--- services/agent/src/config.py
+++ services/agent/src/config.py
@@ -21,10 +21,6 @@ class Config(BaseSettings):
     table_name: str
     embedding_dimensions: int
     time_partition_interval: int
-    supabase_pass: str
-    supabase_url: str
-    supabase_api_key: str
-
 
 config = Config()
 
