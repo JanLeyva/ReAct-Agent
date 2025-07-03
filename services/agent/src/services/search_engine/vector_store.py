96c8ce4fb7ca1cdedef56bcd299cc72bf4b54090
JanLeyva <78868781+JanLeyva@users.noreply.github.com>
2025-07-03 16:55:09 +0000

    style fixes by ruff

--- services/agent/src/services/search_engine/vector_store.py
+++ services/agent/src/services/search_engine/vector_store.py
@@ -244,19 +244,35 @@ class VectorStore:
         Returns:
             A formatted string representation of the search results.
         """
-        def _fill_template(idx, place: pl.Series) -> str: # <--- Type hint here is pl.Series
-            """fill the output template of the agent"""
-            template = f"{idx+1}: <b>{place['name']}</b>: {place['contents']}\n" # <--- Accessing as if it's a dict or Series with string index
-            template += f"<a href='{place['website']}'>website</a> | " if place['website'] is not None else ""
-            template += f"<a href='{place['url']}'>maps</a> | " if place['url'] is not None else ""
-            template += f"{place['international_phone_number']}" if place['international_phone_number'] is not None else ""
-            
-            return template
 
+        def _fill_template(
+            idx, place: pl.Series
+        ) -> str:  # <--- Type hint here is pl.Series
+            """fill the output template of the agent"""
+            template = f"{idx+1}: <b>{place['name']}</b>: {place['contents']}\n"  # <--- Accessing as if it's a dict or Series with string index
+            template += (
+                f"<a href='{place['website']}'>website</a> | "
+                if place["website"] is not None
+                else ""
+            )
+            template += (
+                f"<a href='{place['url']}'>maps</a> | "
+                if place["url"] is not None
+                else ""
+            )
+            template += (
+                f"{place['international_phone_number']}"
+                if place["international_phone_number"] is not None
+                else ""
+            )
+
+            return template
 
         result_formatted = [
             _fill_template(idx, place)
-            for idx, place in enumerate(results.iter_rows(named=True)) # <--- This yields dictionaries
+            for idx, place in enumerate(
+                results.iter_rows(named=True)
+            )  # <--- This yields dictionaries
         ]
 
         return "".join(result_formatted)
